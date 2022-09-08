be86dcad1f0bb0175caa1f3a4767a27a1a5cf472
Edgar Del Valle <edqardv@gmail.com>
2022-09-08 09:38:53 -0700

    Bug changes and latest tests

--- assign1/readyqueue.cpp
+++ assign1/readyqueue.cpp
@@ -4,29 +4,33 @@
 
 using namespace std;
 
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient comments to your code
-
+// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+//  Remember to add sufficient comments to your code
 
 /**
  * @brief Constructor for the ReadyQueue class.
  */
- ReadyQueue::ReadyQueue()  {
-     //TODO: add your code here
-     count = 0;
- }
+ReadyQueue::ReadyQueue()
+{
+    // TODO: add your code here
+    count = 0;
+}
 
 /**
  * @brief Add a PCB representing a process into the ready queue.
  *
  * @param pcbPtr: the pointer to the PCB to be added
  */
-void ReadyQueue::addPCB(PCB *pcbPtr) {
-    //TODO: add your code here
-    // When adding a PCB to the queue, you must change its state to READY.
-    if(count >= MAX){
+void ReadyQueue::addPCB(PCB *pcbPtr)
+{
+    // TODO: add your code here
+    //  When adding a PCB to the queue, you must change its state to READY.
+    if (count >= MAX)
+    {
         cout << "Cannot add to full queue" << endl;
-    } else {
+    }
+    else
+    {
         pcbPtr->setState(ProcState::READY);
         Q[count] = pcbPtr;
         count += 1;
@@ -39,9 +43,10 @@ void ReadyQueue::addPCB(PCB *pcbPtr) {
  *
  * @return PCB*: the pointer to the PCB with the highest priority
  */
-PCB* ReadyQueue::removePCB() {
-    //TODO: add your code here
-    // When removing a PCB from the queue, you must change its state to RUNNING.
+PCB *ReadyQueue::removePCB()
+{
+    // TODO: add your code here
+    //  When removing a PCB from the queue, you must change its state to RUNNING.
     Q[0]->setState(ProcState::RUNNING);
     reheapify();
 }
@@ -51,35 +56,41 @@ PCB* ReadyQueue::removePCB() {
  *
  * @return int: the number of PCBs in the queue
  */
-int ReadyQueue::size() {
-    //TODO: add your code here
+int ReadyQueue::size()
+{
+    // TODO: add your code here
     return count;
 }
 
 /**
  * @brief Display the PCBs in the queue.
  */
-void ReadyQueue::displayAll() {
-    //TODO: add your code here
+void ReadyQueue::displayAll()
+{
+    // TODO: add your code here
 
-    for(int i = 0; i < count; i++){
-        cout << Q[i]->priority << " ";
+    for (int i = 0; i < count; i++)
+    {
+        PCB *currentPCB = Q[i];
+        cout << "ID: " << currentPCB->getID() << ", Priority: " << currentPCB->getPriority() << ", State: " << endl; // << currentPCB->getState() << endl;
     }
-    cout << endl;
 }
 
-void ReadyQueue::swap(PCB* a, PCB* b){
-    PCB* temp = a;
+void ReadyQueue::swap(PCB *a, PCB *b)
+{
+    PCB *temp = a;
     a = b;
     b = temp;
 }
 
-void ReadyQueue::reheapify(){
+void ReadyQueue::reheapify()
+{
     int X = 0;
     Q[X] = Q[count - 1];
     count--;
 
-    while(X < count - 1){
+    while (X < count - 1)
+    {
         int largerChild = getLargerchild(X);
 
         if (largerChild == -1 || Q[largerChild] > Q[X])
@@ -90,39 +101,45 @@ void ReadyQueue::reheapify(){
     }
 }
 
-int ReadyQueue::getLargerchild(int i){
+int ReadyQueue::getLargerchild(int i)
+{
     int LC = 2 * i + 1;
     int RC = 2 * i + 2;
 
-    if (LC > count - 1) 
+    if (LC > count - 1)
         return -1;
-    if (RC > count - 1) 
+    if (RC > count - 1)
         return -1;
-    
+
     if (Q[LC]->priority > Q[RC]->priority)
         return LC;
     else
         return RC;
 }
 
-void ReadyQueue::trickleup(){
+void ReadyQueue::trickleup()
+{
     int X = count - 1;
 
-    while (X > 0){
-        if(Q[getParent(X)]->priority < Q[X]->priority){
+    while (X > 0)
+    {
+        if (Q[getParent(X)]->priority < Q[X]->priority)
+        {
             swap(Q[getParent(X)], Q[X]);
         }
         X = getParent(X);
     }
 }
 
-int ReadyQueue::getParent(int i){
+int ReadyQueue::getParent(int i)
+{
     if (even(i))
         return (i - 1) / 2;
     else
         return (i - 1) / 2 - 1;
 }
 
-bool ReadyQueue::even(int num){
+bool ReadyQueue::even(int num)
+{
     return (num % 2 == 0);
 }
